c467f17a116a2739ed244f670d996605e4e50d04
Ebubekir Bingöloğlu <ebubekir@localhost.localdomain>
2018-12-25 18:34:20 +0300

    controller ve model dosyalari olusturuldu. .htaccess ve default_controller tanimlamalari yapildi.

---
+++ application/controllers/Member.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Member extends CI_Controller{
+
+	public function index(){
+
+		echo "Test";
+	}
+
+	public function registration(){
+
+		echo "deneme";
+	}
+
+	public function activation(){
+
+		
+	}
+}
+
+ ?>
